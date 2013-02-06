5fc406800f22d44f6a6c84bd1621c8e1c65483de
Lefno <jonathanloig@gmail.com>
2013-02-06 02:19:55 +0100

    Añadidos Strings de los EditText

--- gen/curso/postal/bundle1/R.java
+++ gen/curso/postal/bundle1/R.java
@@ -26,6 +26,10 @@ public final class R {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
+        public static final int pass=0x7f040004;
+        /**   String de los EditText 
+         */
+        public static final int usuario=0x7f040003;
     }
     public static final class style {
         /** 
